905b456fd060b27bfb279d0919171741fb7a876a
EdwinDij <edwin.d899@gmail.com>
2023-12-22 17:55:49 +0100

    modif du chat par l'admin

--- backend/Controllers/cat.js
+++ backend/Controllers/cat.js
@@ -2,17 +2,8 @@ import { Cat } from "../Models/cat.js";
 import { User } from "../Models/user.js";
 
 export const createCat = async (req, res) => {
-  const {
-    name,
-    birthday,
-    description,
-    sexe,
-    city,
-    picture,
-    race,
-    isAdopted,
-    userId,
-  } = req.body;
+  const { name, birthday, description, sexe, city, picture, race, isAdopted } =
+    req.body;
 
   try {
     if (req.user.isAdmin) {
@@ -63,6 +54,38 @@ export const deleteCat = async (req, res) => {
     if (req.user.isAdmin) {
       await Cat.destroy({ where: { id: req.params.id } });
       res.status(200).json({ message: "Cat deleted !" });
+    }
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "vous n'avez pas les droits d'admin", error });
+  }
+};
+
+export const updateCat = async (req, res) => {
+  const { name, birthday, description, sexe, city, picture, race, isAdopted } =
+    req.body;
+  try {
+    if (req.user.isAdmin) {
+      Cat.update(
+        {
+          name: name,
+          birthday: birthday,
+          description: description,
+          sexe: sexe,
+          city: city,
+          picture: picture,
+          race: race,
+          isAdopted: isAdopted,
+        },
+        { where: { id: req.params.id } }
+      )
+        .then(() => {
+          res.status(200).json({ message: "Cat updated !" });
+        })
+        .catch((error) => {
+          res.status(400).json({ error });
+        });
     }
   } catch (error) {
     res
--- backend/Routes/cat.js
+++ backend/Routes/cat.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllCats, getOneCat, createCat, deleteCat } from "../Controllers/cat.js";
+import { getAllCats, getOneCat, createCat, deleteCat, updateCat } from "../Controllers/cat.js";
 import { webToken } from "../middlewares/jsonwebtoken.js";
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get("/", getAllCats);
 router.get("/:id", getOneCat);
 router.post("/", webToken, createCat);
 router.delete("/:id", webToken, deleteCat)
+router.put("/:id", webToken, updateCat)
 
 export default router;
